0e49e9aeca4c2165291be3f6feed9cc1eca6a359
Chandana130599 <91872441+Chandana130599@users.noreply.github.com>
2021-10-21 22:00:55 +0530

    Update Base.java

--- Base.java
+++ Base.java
@@ -1,5 +1,4 @@
 package resources;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,32 +9,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
+
 	public WebDriver driver;
 	public Properties prop;
 
-	@SuppressWarnings("deprecation")
+	
 	public WebDriver initializeDriver() throws IOException {
 
 		prop = new Properties();
 		FileInputStream fis = new FileInputStream(
 			   "C:\\Users\\user\\eclipse-workspace\\QTRecognition\\src\\main\\java\\resources\\data.properties");
-
 		prop.load(fis);
 		String browserName = prop.getProperty("browser");
 		System.out.println(browserName);
 
-		if (browserName.equals("chrome")) {
+		if (browserName.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
 			driver = new ChromeDriver();
 			// execute in chrome driver
 
-		} else if (browserName.equals("firefox")) {
+		} else if (browserName.equalsIgnoreCase("firefox")) {
 			driver = new FirefoxDriver();
 			// firefox code
 		} else if (browserName.equals("IE")) {
 //	IE code
 		}
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	
 		return driver;
 	}
 
