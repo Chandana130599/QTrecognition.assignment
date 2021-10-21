7b786d97f67e9d2eb51443df5f69b651037cc3fe
Chandana130599 <91872441+Chandana130599@users.noreply.github.com>
2021-10-21 22:02:35 +0530

    Update KudosSearchPage.java

--- KudosSearchPage.java
+++ KudosSearchPage.java
@@ -1,4 +1,4 @@
-package pageObject;
+package pageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,14 +7,14 @@ import org.openqa.selenium.WebElement;
 public class KudosSearchPage {
 public WebDriver driver;
 	
-	By searchByname = By.id("s_e_add");
+	By searchByname = By.xpath("//*[@id=\"s_e_add\"]");
 	By clickSearchButton = By.xpath("//*[@id=\"search_panel\"]/button");
 	
 public KudosSearchPage(WebDriver driver) {	
 		
 		this.driver=driver;
 	}
-	
+
 	public WebElement getEmail() {
 		return driver.findElement(searchByname);
 	}
@@ -24,4 +24,3 @@ public KudosSearchPage(WebDriver driver) {
 	}
 
 }
-
